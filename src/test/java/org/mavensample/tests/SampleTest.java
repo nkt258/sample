34fc2b347317335026f2240b537a6a2e1e472c52
latha <lathanaveen2016@gmail.com>
2018-05-07 10:32:26 +0530

    nkp commit

---
+++ src/test/java/org/mavensample/tests/SampleTest.java
@@ -0,0 +1,12 @@
+package org.mavensample.tests;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void testB()
+	{
+		System.out.println("hii hellooooo");
+	}
+
+}
